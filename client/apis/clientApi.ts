3ca7c6c86724c9e648378e3a7391f36ebdbe66da
Cong <53280539+Jero327@users.noreply.github.com>
2023-11-15 14:36:07 +1300

    Add new todo

--- client/apis/clientApi.ts
+++ client/apis/clientApi.ts
@@ -1,4 +1,5 @@
 import request from 'superagent'
+import { newTodo } from '../../model/todos'
 
 export async function getTodos() {
   const response = await request.get('/api/v1/todos')
@@ -7,4 +8,13 @@ export async function getTodos() {
 
 export async function deleteTodo(id: number) {
   await request.delete(`/api/v1/todos/${id}`)
+}
+
+export async function addTodo(details: string) {
+  const newTodo = {
+    details: details,
+    priority: null,
+    completed: false,
+  }
+  await request.post(`/api/v1/todos`).send(newTodo)
 }
--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -1,12 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addTodo } from '../apis/clientApi'
+
 // eslint-disable-next-line no-unused-vars
 function AddTodo() {
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: (input: string) => addTodo(input),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['todos'])
+    },
+  })
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+    const input = form.get('newTodo') as string
+    mutation.mutate(input)
+    e.currentTarget.reset()
+  }
+
   return (
     <>
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        autoFocus={true}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="newTodo"
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus={true}
+        />
+      </form>
     </>
   )
 }
