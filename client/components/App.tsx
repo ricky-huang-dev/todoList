a34c0dd5ba3ed4931d5b9514c69ee68975a3df39
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-11-15 13:57:10 +1300

    Move TaskList to its own file

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,46 +1,5 @@
 import AddTodo from './AddTodo.tsx'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { getTasks, toggleCompleted } from '../apis/taskApi.ts'
-import { ITask } from '../../models/taskModel.ts'
-
-function TaskList() {
-  const { data } = useQuery({
-    queryKey: ['tasks'],
-    queryFn: getTasks,
-  })
-
-  const queryClient = useQueryClient()
-
-  const editMutation = useMutation({
-    mutationFn: (task: ITask) => toggleCompleted(task),
-    onSuccess: () => {
-      queryClient.invalidateQueries(['tasks'])
-    },
-  })
-
-  function handleToggleCompleted(task: ITask) {
-    editMutation.mutate(task)
-  }
-
-  return (
-    <ul className="todo-list">
-      {data?.map((task) => (
-        <li key={task.id}>
-          <div className="view">
-            <input
-              className="toggle"
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => handleToggleCompleted(task)}
-            />
-            <label htmlFor="toggle">{task.text}</label>
-            <button name="destroy" className="destroy"></button>
-          </div>
-        </li>
-      ))}
-    </ul>
-  )
-}
+import TaskList from './TaskList.tsx'
 
 function App() {
   return (
